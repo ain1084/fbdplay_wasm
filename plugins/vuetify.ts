aabe465a7f209fe7d1ad2e6048f3159495416c07
Seiji Ainoguchi <seiji.ainoguchi@gmail.com>
2024-06-21 12:41:14 +0000

    reduce bundle size (added vite-plugin-vuetify)

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,12 +1,9 @@
 import { createVuetify } from "vuetify";
 import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
-    components,
-    directives,
+    ssr: true,
     theme: {
       defaultTheme: "dark",
     },
